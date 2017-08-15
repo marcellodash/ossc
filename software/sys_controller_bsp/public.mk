ab78ad96750a079e5953ac26c01d647c187041b3
borti4938 <borti4938@gmx.de>
2017-08-15 10:43:51 +0200

    Simple project setting files updated (no functional updates or bugfixes)

--- software/sys_controller_bsp/public.mk
+++ software/sys_controller_bsp/public.mk
@@ -82,7 +82,7 @@ ACDS_VERSION := 17.0
 
 # This following BUILD_NUMBER comment indicates the build number of the tool 
 # used to generate this makefile. 
-# BUILD_NUMBER: 595
+# BUILD_NUMBER: 598
 
 # Qsys--generated SOPCINFO file. Required for resolving node instance ID's with 
 # design component names. 
